3a03cb23d76aa4a38e415b8c05e5b5f55ca319b0
Victoria <vikaantoniuk@gmail.com>
2022-07-19 19:22:40 +0300

    Refactor sortBy method (support of multiple conditions of the same type)

--- sorting/script.js
+++ sorting/script.js
@@ -10,23 +10,51 @@
 //         return data.filter((el) => el.disabled === disabled);
 // }
 
-Array.prototype.sortBy = function(fieldName) {
-    return this.sort((a, b) => {
-        if (typeof a[fieldName] === 'string') {
-            return a[fieldName].localeCompare(b[fieldName]);
+Array.prototype.sortBy = function(conditions) {
+    let tempArr = this;
 
-        }
+    conditions.forEach((condition) => {
+        tempArr = tempArr.sort((a, b) => {
+            if (typeof a[condition] === 'string') {
+                return a[condition].localeCompare(b[condition]);
 
-        return a[fieldName] - b[fieldName]
-    });
+            }
+
+            return a[condition] - b[condition]
+        });
+    })
+
+    return tempArr;
 };
 
-Array.prototype.include = function(fieldName, value) {
-    return this.filter((el) => el[fieldName] === value);
+Array.prototype.include = function(conditions) {
+    let tempArr = [];
+    console.log('conditions', conditions);
+
+    conditions.forEach((condition) => {
+        const fieldName = Object.keys(condition)[0];
+        const value = condition[Object.keys(condition)[0]];
+        const filteredByCondition = this.filter((el) => el[fieldName] === value);
+
+        tempArr = tempArr.concat(filteredByCondition);
+    })
+
+    console.log('tempArr1', tempArr);
+
+    return tempArr;
 };
 
-Array.prototype.exclude = function (fieldName, value) {
-    return this.filter((el) => el[fieldName] !== value);
+Array.prototype.exclude = function (conditions) {
+    let tempArr = this;
+
+    conditions.forEach((condition) => {
+        const fieldName = Object.keys(condition)[0];
+        const value = condition[Object.keys(condition)[0]];
+
+        tempArr = tempArr.filter((el) => el[fieldName] !== value);
+    })
+
+    return tempArr;
 }
 
 async function getData() {
@@ -38,24 +66,24 @@ getData().then((data) => {
       let checkedData = data;
 
       const condition = {
-          include: [{ name: 'John' }, { name: 'Mike' }],
-          sortBy: ['rating'],
-          exclude: [{ name: 'John'}]
+          include: [{ name: 'John' }, { name: 'Greg' }],
+          sortBy: ['rating', 'name'],
+          exclude: []
       };
 
         Object.keys(condition).forEach((conditionRule) => {
-            let tempData = [];
-            condition[conditionRule].forEach((item) => {
-                console.log('typeof item', typeof item)
-                if (typeof item === 'string') {
-                    checkedData = checkedData[conditionRule](item);
-                } else {
-                    checkedData = checkedData[conditionRule](Object.keys(item)[0], item[Object.keys(item)[0]]);
-                }
-            });
-            checkedData = tempData;
+            checkedData = checkedData[conditionRule](condition[conditionRule]);
+            // condition[conditionRule].forEach((item) => {
+            //     console.log('typeof item', typeof item)
+            //     if (typeof item === 'string') {
+            //         checkedData = checkedData[conditionRule](item);
+            //     } else {
+            //         checkedData = checkedData[conditionRule](item);
+                //        // checkedData = checkedData[conditionRule](Object.keys(item)[0], item[Object.keys(item)[0]]);
+            //     }
+            // });
         })
-        console.log('checkedData', JSON.stringify(checkedData));
+        console.log('checkedData', checkedData);
       // if (condition.sortBy) {
       //     condition.sort_by.forEach((item) => {
       //         checkedData = checkedData.sortBy(item);
